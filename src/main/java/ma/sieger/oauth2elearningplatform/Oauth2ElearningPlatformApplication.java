c18b3f069fba6ba69b67561ec63563cbd26f88e5
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-12-14 14:42:13 +0100

    first commit

---
+++ src/main/java/ma/sieger/oauth2elearningplatform/Oauth2ElearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package ma.sieger.oauth2elearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ElearningPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ElearningPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/sieger/oauth2elearningplatform/Oauth2ElearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sieger.oauth2elearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ElearningPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
